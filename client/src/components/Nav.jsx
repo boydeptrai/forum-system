3e740207f4b313aabaf2823c215b5f00800682ed
Tuan Tran <langthamht047@gmail.com>
2023-03-03 20:11:32 -0800

    update nav

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -1,17 +1,20 @@
-import React from 'react'
+import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const Nav = () => {
-  const signOut = () =>{
-    alert("user signed out!")
-  }
+  const navigate = useNavigate();
+  const signOut = () => {
+    localStorage.removeItem("_id");
+    navigate("/");
+  };
   return (
     <nav className="navbar">
       <h2>Threadify</h2>
       <div className="navbarRight">
-          <button onClick ={signOut}>Sign out</button>
+        <button onClick={signOut}>Sign out</button>
       </div>
     </nav>
-  )
-}
+  );
+};
 
-export default Nav
+export default Nav;
